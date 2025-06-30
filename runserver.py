4a6e5617d924629d26ae6c2bdc1e02a13dda10fe
multazim <multazim978@icloud.com>
2025-06-30 16:00:00 +0200

    init

--- runserver.py
+++ runserver.py
@@ -1,14 +1,10 @@
-"""
-This script runs the FlaskWebProject1 application using a development server.
-"""
-
 from os import environ
 from FlaskWebProject1 import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(environ.get('SERVER_PORT', '8000'))
+        PORT = int(environ.get('PORT', '8000'))  # <-- Use PORT env var, default 8000
     except ValueError:
         PORT = 5555
     app.run(host=HOST, port=PORT)
